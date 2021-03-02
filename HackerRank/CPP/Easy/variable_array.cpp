d1727f9cf35d558e7117c2ad2e5809be85578471
João Canavarro <jvcanavarro@gmail.com>
2021-03-02 13:37:32 -0300

    Finish easy Cpp problemas

--- HackerRank/CPP/Easy/variable_array.cpp
+++ HackerRank/CPP/Easy/variable_array.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 
 int main () {
+    int n, q;
+    cin >> n >> q;
+    vector<vector<int>> ks(n);
+
+
+    for (int i = 0; i < n; i++) {
+        int size;
+        cin >> size;
+        ks[i] = vector<int> (size);
+        for (int j = 0; j < size; j++) {
+            cin >> ks[i][j];
+        }
+        
+    }
+
+    while (q-- > 0) {
+        int x, y;
+        cin >> x >> y;
+        cout << ks[x][y] <<endl;
+    }
 
     return 0;
 }
